6419aa3d8e2d479f7322ada205ec4167e8750544
gnuyhaa <guddk18@naver.com>
2025-08-19 18:11:04 +0900

    sql문 수정

--- ohou.sql
+++ ohou.sql
@@ -1,28 +1,39 @@
-CREATE TABLE `tb_products` (
-	`productID`	INT	NOT NULL,
-	`brand_name`	VARCHAR(255)	NULL,
-	`product_name`	VARCHAR(255)	NULL,
-	`Field`	VARCHAR(255)	NULL
+truncate table tb_products;
+truncate table tb_reviews;
+
+DROP DATABASE ai_reviews;
+
+create database ai_reviews CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+USE ai_reviews;
+
+
+CREATE TABLE tb_products (
+    ID INT NOT NULL AUTO_INCREMENT,
+    productID INT,
+    brand_name VARCHAR(255),
+    product_name VARCHAR(255),
+
+    PRIMARY KEY (ID)
 );
 
--- CREATE TABLE `tb_ai` (
--- 	`category`	VARCHAR(255)	NULL,
--- 	`brand_name`	VARCHAR(255)	NULL,
--- 	`Field3`	VARCHAR(255)	NULL,
--- 	`Field`	VARCHAR(255)	NULL,
--- 	`Field2`	VARCHAR(255)	NULL
--- );
+CREATE TABLE tb_reviews (
+    ID INT NOT NULL AUTO_INCREMENT,
+		goodsID INT NOT NULL,
+		reviewID INT NOT NULL UNIQUE,
+    customerID INT,
+    nickname VARCHAR(255),
+		options VARCHAR(255),
+    grade FLOAT,
+    comment LONGTEXT,
+    event_date DATE,
 
-CREATE TABLE `tb_reviews` (
-	`productID`	INT	NOT NULL,
-	`customerID`	INT	NULL,
-	`nickname`	VARCHAR(255)	NULL,
-	`grade`	FLOAT	NULL,
-	`event_date`	DATE	NULL,
-	`comment`	LONGTEXT	NULL
+    PRIMARY KEY (ID),
+		FOREIGN KEY (goodsID) REFERENCES tb_products(ID)
 );
 
-ALTER TABLE `tb_products` ADD CONSTRAINT `PK_TB_PRODUCTS` PRIMARY KEY (
-	`productID`
-);
 
+SELECT *
+FROM tb_reviews
+WHERE goodsID = 1
+ORDER BY event_date DESC, reviewID DESC;
