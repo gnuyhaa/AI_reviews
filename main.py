5bebda8cea9b018502b2fd8b5e43bae7d9455ebb
gnuyhaa <guddk18@naver.com>
2025-08-20 18:01:16 +0900

    0820 main 실행값 수정

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from DB.db import insert_product, insert_product_review
 from crawler import product_list, product_review
 
 
-insert_product(product_list())
+insert_product(product_list())  # 상품목록 크롤링 + DB 저장
 
-insert_product_review(product_review(pages=3))
+insert_product_review(product_review(pages=5))  # 리뷰 크롤링 + DB 저장
