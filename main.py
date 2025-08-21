463f86c658b94a245bb6983931f53eade6909b93
gnuyhaa <guddk18@naver.com>
2025-08-21 21:38:32 +0900

    0821 main 실행값 수정

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from DB.db import insert_product, insert_product_review
-from crawler import product_list, product_review
+from DB.db import insert_product, insert_product_review, insert_analyze_review, insert_product_list
+from crawler.crawler import product_list, product_review
+from analyzer.ohou_LLM import clean_reviews, analyze_reviews
 
+# 상품 목록 크롤링 + DB 저장
+insert_product(product_list())
 
-insert_product(product_list())  # 상품목록 크롤링 + DB 저장
+# 리뷰 크롤링 + DB 저장
+insert_product_review(product_review(pages=50))
 
-insert_product_review(product_review(pages=5))  # 리뷰 크롤링 + DB 저장
+# 상품목록 전처리 DB 저장
+insert_product_list()
+
+# 리뷰 전처리
+inserted_reviews = clean_reviews()
+# LLM 분석
+all_results = analyze_reviews(inserted_reviews)
+
+# 분석 결과 DB 저장 (reviewID 포함)
+insert_analyze_review(all_results)
+
+print("전체 파이프라인 완료!")
