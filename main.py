1d8ac3335e4a10f4068b54502ce73d3c40ff4a31
gnuyhaa <guddk18@naver.com>
2025-08-25 22:55:07 +0900

    0825 main.py 수정

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
-from DB.db import insert_product, insert_product_review, insert_analyze_review, insert_product_list
+from DB.db import insert_product, insert_product_review, insert_product_list, save_analyze_review
 from crawler.crawler import product_list, product_review
 from analyzer.ohou_LLM import clean_reviews, analyze_reviews
 
-# 상품 목록 크롤링 + DB 저장
+# 1. 상품 목록 크롤링 + DB 저장
 insert_product(product_list())
 
-# 리뷰 크롤링 + DB 저장
-insert_product_review(product_review(pages=50))
+# 2. 리뷰 크롤링 + DB 저장
+insert_product_review(product_review(pages=50)) 
 
-# 상품목록 전처리 DB 저장
+# 3. 상품목록 전처리 DB 저장
 insert_product_list()
 
-# 리뷰 전처리
+# 4. 리뷰 전처리
 inserted_reviews = clean_reviews()
-# LLM 분석
+
+# 5. LLM 분석
 all_results = analyze_reviews(inserted_reviews)
 
-# 분석 결과 DB 저장 (reviewID 포함)
-insert_analyze_review(all_results)
+# 6. 분석 결과 DB 저장 (reviewID 포함)
+save_analyze_review(all_results)
 
 print("전체 파이프라인 완료!")
